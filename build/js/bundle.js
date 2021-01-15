694cab6559ca002b9d72d57218a893fe99bcfaf3
elisa33 <pipi.tdfuego@gmail.com>
2021-01-15 10:49:51 -0400

    scroll nav effects

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,4 +1,37 @@
-console.log('listo');
+document.addEventListener('DOMContentLoaded', function () {
+	scrollNav();
+	navegacionFija();
+});
+
+function navegacionFija() {
+	const barra = document.querySelector('.header');
+	//registrar el intersection observer
+	const observer = new IntersectionObserver(function (entries) {
+		if (entries[0].isIntersecting) {
+			barra.classList.remove('fijo');
+		} else {
+			barra.classList.add('fijo');
+		}
+	});
+	//elemento a observar
+	observer.observe(document.querySelector('.sobre-festival'));
+}
+
+function scrollNav() {
+	const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+	enlaces.forEach(function (enlace) {
+		enlace.addEventListener('click', function (e) {
+			e.preventDefault();
+			const seccion = document.querySelector(e.target.attributes.href.value);
+			console.log(seccion);
+
+			seccion.scrollIntoView({
+				behavior: 'smooth',
+			});
+		});
+	});
+}
 
 document.addEventListener('DOMContentLoaded', function () {
 	crearGaleria();
